2369c363737cc3deea5c2c7407d52b90739010f8
LonnieA123 <lonnie.j.arellano@gmail.com>
2023-09-06 14:26:56 -0500

    connection to database

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose').default
 const workoutRoutes = require('./routes/workouts')
 require('dotenv').config();
 
-//express app
+// express app
 const app = express()
 
 
 app.use(express.json())
-//logs requests
+// logs requests
 app.use((req,res,next) => {
     console.log(req.path, req.method)
     next()
 })
 
-//routes
+// routes
 app.use('/api/workouts',workoutRoutes)
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port ', process.env.PORT)
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('listening on port ', process.env.PORT)
+        })
+
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+
 
