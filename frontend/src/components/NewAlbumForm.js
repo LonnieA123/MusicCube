b76d2fc47e90b84f8abf59553c0efa2d51e7155c
LonnieA123 <lonnie.j.arellano@gmail.com>
2023-09-29 21:37:39 -0500

    created state for album form

--- frontend/src/components/NewAlbumForm.js
+++ frontend/src/components/NewAlbumForm.js
@@ -1,4 +1,10 @@
+import {useState} from "react";
+
 const NewAlbumForm = () => {
+    const [title, setTitle] = useState('');
+    const [artist, setArtist] = useState('');
+    const [cover, setCover] = useState('');
+
 
     return(
         <div></div>
